38d8100b7427a49f9baa75baf35ed1595c2cd695
Cat <catb0591@gmail.com>
2021-12-05 17:45:52 -0500

    pages functioning w.out CSS

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable padding-line-between-statements */
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -10,7 +12,27 @@ response.render('mainpage', {shows})
 })
 
 app.get('/season/1', (request, response) => {
-    response.render('seasonpage', { shows })
+    const season= shows.seasons[0]
+    response.render('seasonpage', {season})
+})
+
+app.get('/season/2', (request, response) => {
+    const season = shows.seasons[1]
+    response.render('seasonpage', {season})
+})
+
+app.get('/season/3', (request, response) => {
+    const season = shows.seasons[2]
+    response.render('seasonpage', {season})
+})
+
+app.get('/season/4', (request, response) => {
+    const season = shows.seasons[3]
+    response.render('seasonpage', {season})
+})
+
+app.all('*', (request, response) => {
+    return response.status(404).send('Sorry wrong page')
 })
 
 app.listen(1337, () => {
