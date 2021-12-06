de4d7ea18417b9c8842057b5e0dcf196dbbb1dcf
Cat <catb0591@gmail.com>
2021-12-05 20:11:50 -0500

    Main page completed

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ const app = express()
 
 const shows = require('./showdata.js')
 
+app.use(express.static('public'))
+
 app.set('view engine', 'pug')
 
 app.get('/', (request, response) => {
-response.render('mainpage', {shows})
+    response.render('mainpage', { shows })
 })
 
 app.get('/season/1', (request, response) => {
-    const season= shows.seasons[0]
-    response.render('seasonpage', {season})
+    const season = shows.seasons[0]
+    response.render('seasonpage', { season })
 })
 
 app.get('/season/2', (request, response) => {
     const season = shows.seasons[1]
-    response.render('seasonpage', {season})
+    response.render('seasonpage', { season })
 })
 
 app.get('/season/3', (request, response) => {
     const season = shows.seasons[2]
-    response.render('seasonpage', {season})
+    response.render('seasonpage', { season })
 })
 
 app.get('/season/4', (request, response) => {
     const season = shows.seasons[3]
-    response.render('seasonpage', {season})
+    response.render('seasonpage', { season })
 })
 
 app.all('*', (request, response) => {
@@ -36,7 +38,6 @@ app.all('*', (request, response) => {
 })
 
 app.listen(1337, () => {
-    console.log ('listening on 1337...)')
+    console.log('listening on 1337...)')
 })
 
-app.use(express.static('public'))
